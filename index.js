e0327b100df2f43117e5abc5721bec1140a5bfa8
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-03-10 19:53:20 -0400

    upd
    prepared

---
+++ index.js
@@ -0,0 +1,7 @@
+// const nodemon = require("nodemon"); <= import from package
+
+const obj = require("./products");
+
+console.log(obj);
+const { products } = require("./products");
+console.log(products);
---
+++ products.js
@@ -0,0 +1,10 @@
+const products = ["lenovo", "Apple"];
+const categories = ["notebooks", "watches"];
+
+const func = () => {};
+
+module.exports = {
+  products,
+  categories,
+  func,
+};
